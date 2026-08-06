e3c9db72865b2045c06aa92f2e7190d271048db9
RafaelAroeira97 <rafaaroeira@hotmail.com>
2026-08-06 20:34:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StudioAroeiraOS from "./App.jsx";
+import StudioAroeiraOS, { CatalogoModelos } from "./App.jsx";
+
+const ehCatalogo = new URLSearchParams(window.location.search).get("catalogo") === "1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StudioAroeiraOS />
+    {ehCatalogo ? <CatalogoModelos /> : <StudioAroeiraOS />}
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`).catch(() => {});
+  });
+}
